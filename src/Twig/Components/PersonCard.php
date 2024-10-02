70edef67a79f03a35dbdbb83c8f7f70b361853b3
Felix Eymonot <felix.eymonot@gmail.com>
2024-10-02 14:48:09 +0200

    feat(person) - handle default values with component

--- src/Twig/Components/PersonCard.php
+++ src/Twig/Components/PersonCard.php
@@ -4,11 +4,26 @@ namespace App\Twig\Components;
 
 use App\Entity\Person;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\PostMount;
 
 #[AsTwigComponent(template: 'components/person_card.html.twig')]
 final class PersonCard
 {
-    public Person $person;
+    public ?Person $person = null;
+
+    #[PostMount]
+    public function setDefaultValues(): void
+    {
+        if ($this->person === null) {
+            $this->person = new Person(
+                'Doe',
+                'John',
+                'https://static-00.iconduck.com/assets.00/avatar-default-icon-2048x2048-h6w375ur.png',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                '#'
+            );
+        }
+    }
 
     public function getPersonFullName(): string
     {
